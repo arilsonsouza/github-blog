73ba9aef676a40c6f3a57987baecb6ea125c54e9
Arilson Souza <arilsons61@gmail.com>
2023-12-07 22:39:18 -0300

    feat: add loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,18 @@
+import { LoadingContainer, LoadingWrapper } from './styles'
+
+export function Loading() {
+  return (
+    <LoadingContainer>
+      <LoadingWrapper>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </LoadingWrapper>
+    </LoadingContainer>
+  );
+}
